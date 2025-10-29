5e34b43fb97e896b52ff8e78c0bf1540e6a8f151
Santanakamile <santana.kamile@escola.pr.gov.br>
2025-10-29 12:01:27 -0300

    kamis

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ console.log(avanca)
 
 avanca.forEach(button=>){
     button.addEventListener('click',function())
-    const atual
+    const atual=document.querySelector("ativo")
+    const proximoPasso="passo"+getAttribute("data-proximo")
+    atual.classList.remove("ativo")
+    document.getElementById(proximoPasso).classList.add(ativo)
